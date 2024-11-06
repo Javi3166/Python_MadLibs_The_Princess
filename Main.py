bf1b3a3464111013a10decdadedf2ebcafb66e4a
Emilio <javi3166@yahoo.com>
2024-11-06 14:52:17 -0500

    Initial commit and final code

---
+++ Main.py
@@ -0,0 +1,17 @@
+adjective = input("Please pick an adjective: ")
+name_person = input("Please pick a name: ")
+name_country = input("Please pick a name for the country: ")
+name_kingdom = input("Please pick a name for the kingdom: ")
+plural_nouns = input("Please pick plural nouns: ")
+verb = input("Please pick a verb: ")
+color = input("Please pick a color: ")
+noun = input("Please pick a noun: ")
+plural_verbs = input("Please pick plural verbs: ")
+
+print("\nOnce a upon a time, there was a " + adjective + " princess named " + name_person + ". \nShe was from " + name_country
+      + ". \nShe lived in a beautiful kingdom on the hills called " + name_kingdom + ". \nShe had five " + plural_nouns +
+      ". \nWhile she was " + verb + " the horse she fell off the horse and her dress had a " + color +
+      " stain on it. \nWhen she got back to the kingdom she quickly changed so she could wash her dress. \nHer " + noun +
+      " told her to come down for dinner. \nAfter they finished eating they all went to take " + plural_verbs +
+      ". \nThe following day the princess went out to the village to go to the supermarket when she ran into a prince. "
+      "\nThey fell in love and got married and had three children and they lived happily ever after.")
